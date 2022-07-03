e99dd9c67be48d64516cc3f4abe03da996a97130
Datureli <soleris1@gmail.com>
2022-07-03 15:51:43 +0200

    commit changes

--- tests/unit/foo.spec.js
+++ tests/unit/foo.spec.js
@@ -1,20 +1,15 @@
 import { shallowMount } from "@vue/test-utils";
-import Login from "@/components/login/Login";
+import Products from "@/components/Products";
 
 describe("login button test", () => {
   it("renders message when component is created", () => {
     // render the component
-    const wrapper = shallowMount(Login, {
+    const wrapper = shallowMount(Products, {
       propsData: {
-        username: "",
-        password: "",
-        elementVisible: true,
-        dialog: false,
-        showPassword: false,
-        loggedIn: false,
+
       },
     });
-    expect(wrapper.vm.$options.name).toMatch("Login");
+    expect(wrapper.vm.$options.name).toMatch("Products");
 
 
   });
