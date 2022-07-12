9206f27a9ec759ae29e142d63faba526b676d81c
Datureli <soleris1@gmail.com>
2022-07-12 15:52:17 +0200

    tests - add item to cart test

--- tests/unit/products.spec.js
+++ tests/unit/products.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import Products from "@/components/Products";
+import App from "@/App"
 
 describe("addToCart Component", () => {
   it("Check if product is emitted corectlly", async () => {
@@ -12,5 +13,8 @@ describe("addToCart Component", () => {
     expect(wrapper.emitted().product).toBeTruthy();
 
     expect(shallowMount(Products).element).toMatchSnapshot();
-  });
+  })
+  it("add item to cart", () => {
+    const wrapper = shallowMount(App);
+  })
 });
